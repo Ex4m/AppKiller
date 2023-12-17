512efc1e50a137012c152cd90737df0f0575c5cb
Ex4m <121984532+Ex4m@users.noreply.github.com>
2023-12-18 00:20:56 +0100

    Graphics update
    - sections work now as individuals
    - corresponding blur / color for active/inactive cells

--- AppKiller.py
+++ AppKiller.py
@@ -1,10 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton, QCheckBox
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QCheckBox
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QCheckBox, QSizePolicy
 import psutil
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
+import re
 
 
 def app_killer(exe_to_kill, path_contains):
@@ -28,70 +25,109 @@ def app_killer(exe_to_kill, path_contains):
 app_killer("NiceTaskbar.exe", "9am")
 
 
+class Section(QFrame):
+    def __init__(self, numbering):
+        super().__init__()
+
+        self.setFrameShape(QFrame.Box)
+        self.setFrameShadow(QFrame.Raised)
+        self.setLineWidth(2)  # Nastaví šířku rámečku
+
+        self.edit1 = QLineEdit('Perl.exe')
+        self.edit2 = QLineEdit('serverApp')
+
+        # Vytvoření grid layoutu pro jednu sekci
+        self.layout = QGridLayout(self)
+
+        self.label = QLabel(str(numbering) + '. Inactive')
+        self.checkbox = QCheckBox()
+
+        self.layout.addWidget(self.label, 0, 0)
+        self.layout.addWidget(self.checkbox, 0, 1)
+        self.layout.addWidget(QLabel('Exe to Kill:'), 1, 0)
+        self.layout.addWidget(self.edit1, 1, 1)
+        self.layout.addWidget(QLabel('Path contains:'), 2, 0)
+        self.layout.addWidget(self.edit2, 2, 1)
+
+        # Nastavení počátečního stavu checkboxu a vstupních polí
+        self.checkbox.setChecked(True)
+        self.toggle_widgets_enabled()
+        self.toggle_checkbox()  # Přidá 'Active' nebo 'Inactive' do textu
+
+        # Přidání reakce na kliknutí na checkbox (až po inicializaci)
+        self.checkbox.clicked.connect(self.toggle_checkbox)
+        # Volání toggle_checkbox pro inicializaci textu a efektů
+        self.toggle_checkbox()
+
+    def toggle_checkbox(self):
+        # Metoda pro přepnutí stavu checkboxu
+        self.toggle_widgets_enabled()
+        label_text = re.sub(r'\b(?:Active|Inactive)\b',
+                            '', str(self.label.text()))
+        if self.checkbox.isChecked():
+            self.label.setText(label_text + ' Active')
+            self.removeBlurEffect()
+            self.setStyleSheet("background-color: #d0ffd0;")
+        else:
+            self.label.setText(label_text + ' Inactive')
+            self.addBlurEffect()
+            self.setStyleSheet("background-color: #ffd0d0;")
+
+    def toggle_widgets_enabled(self):
+        # Metoda pro povolení/disablování vstupních polí podle stavu checkboxu
+        enable_widgets = self.checkbox.isChecked()
+        self.edit1.setEnabled(enable_widgets)
+        self.edit2.setEnabled(enable_widgets)
+
+    def addBlurEffect(self):
+        # Přidání efektu rozmazání
+        blur = QGraphicsBlurEffect()
+        blur.setBlurRadius(1.5)  # Nastavte poloměr rozmazání podle potřeby
+        self.setGraphicsEffect(blur)
+
+    def removeBlurEffect(self):
+        # Odebrání efektu rozmazání
+        self.setGraphicsEffect(None)
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(800, 400, 300, 300)
+        self.setWindowTitle("App Killer")
+
+        self.sections = []
+
+        self.mainLayout = QVBoxLayout(self)
+        self.lowerMenuLayout = QHBoxLayout(self)
+
+        self.add_section()  # 1st sekce
+        self.add_section()  # 2nd sekce
+
+        self.sec_button = QPushButton('Add Section')
+        self.sec_button.clicked.connect(self.add_section)
+        self.lowerMenuLayout.addWidget(self.sec_button)
+
+        self.exp_button = QPushButton('Export')
+        self.exp_button.clicked.connect(self.export_exe)
+        self.lowerMenuLayout.addWidget(self.exp_button)
+        self.mainLayout.addLayout(self.lowerMenuLayout)
+        self.setLayout(self.mainLayout)
+
+    def add_section(self):
+        section = Section(len(self.sections) + 1)
+        section.setMaximumHeight(80)
+        self.mainLayout.insertWidget(self.mainLayout.count() - 1, section)
+        self.sections.append(section)
+
+    def export_exe(self):
+        pass
+
+
 def main():
     app = QApplication([])
-    window = QWidget()
-    window.setGeometry(800, 400, 300, 300)
-    window.setWindowTitle("Resizable Sizer")
-
-    sections = []
-
-    def app_section(numbering):
-        # Funkce pro vytvoření jedné sekce
-        edit1 = QLineEdit('Perl.exe')
-        edit2 = QLineEdit('serverApp')
-
-        # Vytvoření horizontálního uspořádání
-        sectionLayout = QHBoxLayout()
-        l0_v_layout = QVBoxLayout()
-        l1_v_layout = QVBoxLayout()
-        l2_v_layout = QVBoxLayout()
-
-        l0_v_layout.addWidget(QLabel(str(numbering) + '.'))
-        l0_v_layout.addWidget(QCheckBox())
-        l1_v_layout.addWidget(QLabel('Exe to Kill:'))
-        l1_v_layout.addWidget(edit1)
-        l2_v_layout.addWidget(QLabel('Path contains:'))
-        l2_v_layout.addWidget(edit2)
-
-        sectionLayout.addLayout(l0_v_layout)
-        sectionLayout.addLayout(l1_v_layout)
-        sectionLayout.addLayout(l2_v_layout)
-
-        return sectionLayout
-
-    numbering = 1
-
-    def add_section():
-        # Funkce pro přidání další sekce
-        nonlocal numbering
-        sectionLayout = app_section(numbering)
-        sectionWidget = QWidget()
-        sectionWidget.setLayout(sectionLayout)
-        sectionWidget.setMaximumHeight(55)
-        mainLayout.insertWidget(mainLayout.count() - 1,
-                                sectionWidget)  # Vložení před tlačítko
-        sections.append(sectionWidget)
-        numbering += 1
-
-    mainLayout = QVBoxLayout()
-
-    # Přidat tlačítko na začátek layoutu pro přidání sekce
-    button = QPushButton('Add Section')
-    button.clicked.connect(add_section)
-    mainLayout.addWidget(button)
-
-    add_section()  # 1st def sekce
-    add_section()  # 2nd def sekce
-
-    # Nastavení maximální výšky pro QHBoxLayout
-    # section_h_layout = QHBoxLayout()
-    # section_h_layout.addLayout(app_section())
-    # mainLayout.addLayout(section_h_layout)
-
-    # Nastavení uspořádání pro hlavní okno
-    window.setLayout(mainLayout)
-
+    window = MainWindow()
     window.show()
     app.exec_()
 
