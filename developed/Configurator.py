dd898aaaaf5d6a1a03d753c6f3987190e913f77c
OZ <ondrej.zelik@gatema.cz>
2024-03-07 15:16:52 +0100

    Inverted button + help
    - inverted btn - spec background color and logic added
    - help reworked without Qmessage so it has better alignment

--- developed/Configurator.py
+++ developed/Configurator.py
@@ -12,6 +12,7 @@ class Section(QFrame):
     def __init__(self, numbering):
         super().__init__()
         self.numbering = numbering
+        self.boxColor = ''
 
         self.setFrameShape(QFrame.Box)
         self.setFrameShadow(QFrame.Raised)
@@ -28,20 +29,30 @@ class Section(QFrame):
         self.label = QLabel(str(numbering) + '. Inactive')
         self.checkbox = QCheckBox()
 
+        self.invert_label = QLabel("Inverse logic for killing:")
+        self.invert_chkbox = QCheckBox()
+
         self.layout.addWidget(self.label, 0, 0)
         self.layout.addWidget(self.checkbox, 0, 1)
+        self.layout.addWidget(self.invert_label, 0, 2)
+        self.layout.addWidget(self.invert_chkbox, 0, 3)
         self.layout.addWidget(QLabel('Exe to Kill:'), 1, 0)
-        self.layout.addWidget(self.edit1, 1, 1)
+        self.layout.addWidget(self.edit1, 1, 1, 1, 3)
         self.layout.addWidget(QLabel('Path contains:'), 2, 0)
-        self.layout.addWidget(self.edit2, 2, 1)
+        self.layout.addWidget(self.edit2, 2, 1, 2, 3)
 
         # Nastavení počátečního stavu checkboxu a vstupních polí
         self.checkbox.setChecked(True)
         self.toggle_widgets_enabled()
         self.toggle_checkbox()  # Přidá 'Active' nebo 'Inactive' do textu
 
+        # Nastavení počátečního stavu inv checkboxu a změny barvy po zaškrtnutí
+        self.invert_chkbox.setChecked(False)
+
         # Přidání reakce na kliknutí na checkbox (až po inicializaci)
         self.checkbox.clicked.connect(self.toggle_checkbox)
+        self.invert_chkbox.clicked.connect(self.toggle_inv_chkbox)
+
         # Volání toggle_checkbox pro inicializaci textu a efektů
         self.toggle_checkbox()
 
@@ -50,14 +61,27 @@ class Section(QFrame):
         self.toggle_widgets_enabled()
         label_text = re.sub(r'\b(?:Active|Inactive)\b',
                             '', str(self.label.text()))
-        if self.checkbox.isChecked():
-            self.label.setText(label_text + 'Active')
-            # self.removeBlurEffect()
-            self.setStyleSheet("background-color: #d0ffd0;")
+        self.label.setText(label_text + 'Active')
+        # self.removeBlurEffect()
+        if self.invert_chkbox.isChecked() and self.checkbox.isChecked():
+            self.boxColor = '#F8C754'
+            self.setStyleSheet(
+                f"background-color: {self.boxColor};")  # yellow
+        elif self.checkbox.isChecked():
+            self.boxColor = '#d0ffd0'
+            self.setStyleSheet(
+                f"background-color: {self.boxColor};")  # green
         else:
+            self.boxColor = '#ffd0d0'
             self.label.setText(label_text + 'Inactive')
             # self.addBlurEffect()
-            self.setStyleSheet("background-color: #ffd0d0;")
+            self.setStyleSheet(f"background-color: {self.boxColor};")  # red
+
+    def toggle_inv_chkbox(self):
+        if self.invert_chkbox.isChecked() and self.checkbox.isChecked():
+            self.setStyleSheet("background-color: #F8C754;")  # yellow
+        if not self.invert_chkbox.isChecked():
+            self.setStyleSheet(f"background-color: {self.boxColor}")
 
     def toggle_widgets_enabled(self):
         # Metoda pro povolení/disablování vstupních polí podle stavu checkboxu
@@ -133,13 +157,13 @@ class MainWindow(QWidget):
         self.exp_button = QPushButton('Export as')
         self.exp_button.clicked.connect(self.export)
         self.lowerMenuLayout.addWidget(self.exp_button)
-       
+
         self.help_button = QPushButton('Help')
         self.help_button.clicked.connect(self.help)
         # self.help_button.setFixedWidth(120)
         self.lowerMenuLayout.addWidget(self.help_button)
         self.setLayout(self.mainLayout)
-        
+
     def get_dir_based_on_file(self):
         if getattr(sys, 'frozen', False):
             # for .exe
@@ -147,12 +171,12 @@ class MainWindow(QWidget):
         else:
             # for .py
             script_dir = os.path.dirname(os.path.realpath(__file__))
-        
+
         return script_dir
-        
+
     def add_section(self):
         section = Section(len(self.sections) + 1)
-        section.setMaximumHeight(80)
+        section.setMaximumHeight(95)
         self.sectionLayout.addWidget(section)
         self.sections.append(section)
 
@@ -177,11 +201,20 @@ class MainWindow(QWidget):
         return True
 
     def help(self):
-        note = []
-        note.append(';;  -->  for split paths within one cell             ')
-        str_note = "".join(note)
-        MessageHandler.show_information(str_note)
-        
+        msg1 = []
+        msg2 = []
+        separator = "  -->  "
+        msg1.append(';;')
+        msg2.append('for split paths within one cell')
+
+        msg1.append('Inverse')
+        msg2.append(
+            'Inverse logic for killing apps. (i.e. what not to be killed)')
+
+        # msg1_note = "".join(msg1)
+        # msg2_note = "".join(msg2)
+        MessageHandler.show_help(msg1, msg2, separator)
+
     def locate_file(self, file, current_directory):
         for root, directory, files in os.walk(current_directory):
             if file in files:
@@ -286,6 +319,43 @@ class MessageHandler:
         msg_box.setWindowTitle("Information")
         msg_box.setText(message)
         msg_box.exec_()
+
+    @staticmethod
+    def show_help(message1, message2, separator, parent=None):
+        dialog = QDialog(parent)
+        dialog.setWindowTitle("Help")
+
+        help_layout = QVBoxLayout(dialog)
+        grid_layout = QGridLayout()
+
+        for index, msg in enumerate(message1):
+            label = QLabel(msg)
+            grid_layout.addWidget(label, index, 0)
+
+            sep = QLabel(separator)
+            grid_layout.addWidget(sep, index, 1)
+
+        for index, msg in enumerate(message2):
+            label = QLabel(msg)
+            grid_layout.addWidget(label, index, 2)
+
+        # Přidání grid layoutu do hlavního layoutu
+        help_layout.addLayout(grid_layout)
+
+        ok_button = QPushButton("OK", dialog)
+        ok_button.setFixedWidth(80)
+        ok_button.clicked.connect(dialog.accept)
+
+        # Vytvoření horizontálního layoutu pro vycentrování tlačítka
+        hbox = QHBoxLayout()
+        hbox.addStretch()  # Přidá pružný prostor před tlačítkem
+        hbox.addWidget(ok_button)
+        hbox.addStretch()  # Přidá pružný prostor za tlačítkem
+
+        # Přidání horizontálního layoutu do vertikálního layoutu
+        help_layout.addLayout(hbox)
+        dialog.setLayout(help_layout)
+        dialog.exec_()
 
     @staticmethod
     def show_question(message, parent=None, ):
